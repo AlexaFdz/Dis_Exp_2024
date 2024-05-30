d37f7d56dc3b4691d8a2806dc13e330f53496d17
AlexaFdz <aalexa.fdz95@gmail.com>
2024-05-30 12:07:18 -0600

    clase 4 del ANOVA

--- CLASS/clase_4.R
+++ CLASS/clase_4.R
@@ -12,30 +12,31 @@ die4 <- c(1.6, 2.1, 2.1, 2.4)
 
 # Sumatoria de grupos/bloques
 # Para peso bajo sumar la ganancia en peso
-sum(diel[1]+die2[1]+die3[1]+die4[1])
-sum(diel[2]+die2[2]+die3[2]+die4[2])
-sum(diel[3]+die2[3]+die3[3]+die4[3])
-sum(diel[4]+die2[4]+die3[4]+die4[4])
+sum(die1[1]+die2[1]+die3[1]+die4[1])
+sum(die1[2]+die2[2]+die3[2]+die4[2])
+sum(die1[3]+die2[3]+die3[3]+die4[3])
+sum(die1[4]+die2[4]+die3[4]+die4[4])
 
 # sumatoria de las dietas independienetes de grupo/bloque
 sum(die1); sum(die2); sum(die3); sum(die4)
 
 GP <- c(die1, die2, die3, die4)
-GP <- c(2.4, 2.2, 3.4, 1.6, 2.2, 1.9, 1.7, 2.1,
-        3.3, 1.3, 2.8, 2.1, 1.6, 2.5, 1.4, 2.4)
-trat <- gl(4,4,16, labels=c(die1, die2, die3, die4))
-bloque <- gl (4,4,16, labels = c("bajo", "normal", "SP", "OB"))
+Trat <- gl(4,4,16, labels = c("die1","die2","die3","die4"))
+Bloq <- gl(4,4,16, labels = c("Bajo","Normal","SP", "OB"))
 
-dietas <- data.frame(Trat, GP, bloque)
-head(dietas)
+Dietas <- data.frame(Trat,Bloq,GP)
+head(Dietas)
 
-boxplot( dietas$GP ~ dietas$Trat,
-         "col" = "salmon", 
-         "xlab" = "dietas", 
-         "ylab" = "ganancia en peso (Kg)")
-tapply(dietas$GP, dietas$Trat, var)
-fligner.test(dietas$GP, dietas$Trat)         
-bartlett.test(dietas$GP, dietas$Trat)         
+boxplot(Dietas$GP~Dietas$Trat,
+        col = "black",
+        xlab = "Dietas",
+        ylab = "Ganancia en peso (Kg)")
 
-diet.aov <- aov(dietas$GP ~ dietas$Trat)
+tapply(Dietas$GP, Dietas$Trat, var)
+
+fligner.test(Dietas$GP, Dietas$Trat)
+bartlett.test(Dietas$GP, Dietas$Trat)
+
+diet.aov <- aov(Dietas$GP~Dietas$Trat)
 summary(diet.aov)
+
